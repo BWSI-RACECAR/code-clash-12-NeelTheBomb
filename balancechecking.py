970352e64ca3d75f72c1e27a0b776ea8bc89c357
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-27 14:00:06 -0400

    Update balancechecking.py

--- balancechecking.py
+++ balancechecking.py
@@ -1,27 +1,24 @@
 class Solution:
     def isBalanced(self, parenthesis): 
-            #type parenthesis: string
-            #return type: boolean
-            dict = {"}":"{", "]":"[",")":"(", "{":"}", "[":"]","(":")"}
-            list = []
-            if len(parenthesis) <= 1:
-                return False
-            for c in parenthesis:
-                if c in dict:
-                    list.append(c)
-            print(list)
-            #TODO: Write code below to returnn a boolean value with the solution to the prompt. 
-            count = 0
-            print(parenthesis)
-            for e in range(len(list)):
-                count = 0
-                char = parenthesis[e]
-                for i in range(e, len(list)):
-                    if ((char == dict[list[i]]) and (e != i)):
-                         count = count+1
-                if count == 0:
-                    return False
+        breckets = 0
+        parens = 0
+        curly = 0
+        if "{" in parenthesis:
+            curly = curly + 1
+        if "}" in parenthesis:
+            curly -= 1
+        if "[" in parenthesis:
+            brackets += 1
+        if "]" in parenthesis:
+            brackets -= 1
+        if "(" in parenthesis:
+            parens += 1
+        if ")" in parenthesis:
+            parens -= 1
+        if brackets == 0 and parens == 0 and curly == 0:4
             return True
+        return False
+        
                     
                 
             pass
