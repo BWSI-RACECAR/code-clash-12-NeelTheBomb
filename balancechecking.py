e61d5a3bb442554484f60e549694a7a99b6d6176
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-27 09:05:03 -0400

    Add files via upload

---
+++ balancechecking.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isBalanced(self, parenthesis): 
+            #type parenthesis: string
+            #return type: boolean
+            
+            #TODO: Write code below to returnn a boolean value with the solution to the prompt. 
+            count = 0
+            for e in range(len(parenthesis)):
+                count = 0
+                char = parenthesis[e]
+                for i in range(len(parenthesis)):
+                    if (parenthesis[i] == parenthesis[e]) and (e != i):
+                         count = count+1
+                if count == 0:
+                    return False
+            return True
+                    
+                
+            pass
+
+def main():
+    str1=input()
+    tc1= Solution()
+    ans=tc1.isBalanced(str1)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
